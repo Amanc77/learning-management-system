f96cde0a96f6a0707cf2b92c51ea96e100f79afd
Aman Kumar Chaurasiya <amanchaurasiya255@gmail.com>
2025-08-05 22:22:45 +0530

    auth

--- client/src/components/common-form/index.jsx
+++ client/src/components/common-form/index.jsx
@@ -1,7 +1,32 @@
-function CommonForm({ handleSubmit, buttonText }) {
+function CommonForm({
+  handleSubmit,
+  buttonText,
+  formControls = [],
+  formData,
+  setFormData,
+}) {
   return (
     <form onSubmit={handleSubmit}>
-      <button>{buttonText || "Submit"}</button>
+      {/* Form Controls */}
+      <FormControls
+        formControls={formControls}
+        formData={formData}
+        setFormData={setFormData}
+      />
+
+      {/* Additional Form Elements */}
+      <div className="flex flex-col gap-3">
+        {formControls.length > 0
+          ? renderComponentByType()
+          : "No controls to submit"}
+      </div>
+
+      {/* Submit Button */}
+      <div className="flex justify-end mt-4">
+        <button type="submit" className="btn btn-primary">
+          {buttonText || "Submit"}
+        </button>
+      </div>
     </form>
   );
 }
