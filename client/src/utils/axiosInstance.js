545fd5a4d75a549e11f6dede29dafad009bb4c9b
Aman Kumar Chaurasiya <amanchaurasiya255@gmail.com>
2025-09-01 00:50:28 +0530

    .

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
-  withCredentials: true, // MUST be true to send cookies
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true, // send cookies if needed
   headers: {
     "Content-Type": "application/json",
   },
